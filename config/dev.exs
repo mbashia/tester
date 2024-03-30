beeb4b26cda3cb15c9a99d191522f4d3de2ce9a0
root <root@vmi1648040.contaboserver.net>
2024-03-30 09:05:22 +0000

    added

--- config/dev.exs
+++ config/dev.exs
@@ -18,7 +18,7 @@ config :tester, Tester.Repo,
 config :tester, TesterWeb.Endpoint,
   # Binding to loopback ipv4 address prevents access from other machines.
   # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
-  http: [ip: {127, 0, 0, 1}, port: 4000],
+  http: [port: 5700],
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
